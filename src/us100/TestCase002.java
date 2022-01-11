c68f583d630cfe34c7ae487e3cffb0098561e71f
elifdastan <97212348+elifdastan@users.noreply.github.com>
2022-01-11 08:58:58 -0800

    started Test case 002 step 2 is done

--- src/us100/TestCase002.java
+++ src/us100/TestCase002.java
@@ -2,6 +2,10 @@ package us100;
 
 public class TestCase002 {
 
+    public static void main(String[] args) {
+        System.out.println("practice2");
+    }
+
     // step 1 is done
 
     // step 2 is done
